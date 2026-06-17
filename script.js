f67c968d73a4ae58d76c52cc77f84ed136d2e819
Murilo <murilobarachor@gmail.com>
2026-06-17 20:11:44 -0300

    commit-1

---
+++ script.js
@@ -0,0 +1,196 @@
+// Importar todas as funções que foram exportadas no script_db.js
+import {
+consultarDiretoComFetch,
+insertTarefa,
+sqlAtualizarTarefa,
+sqlDeletarTarefa
+} from './script_db.js'
+
+// Busca os Elementos Principais do HTML pelo ID e guarda em uma variável
+const formTarefa = document.getElementById('form-tarefa');
+const tabelaTarefa = document.getElementById('tabela-corpo');
+const btnCancelar = document.getElementById('btn-cancelar');
+const formTitulo = document.getElementById('form-titulo');
+const btnSalvarText = document.getElementById('btn-salvar-text');
+
+//================================
+// Funções Visuais, Auxiliares
+//================================
+
+function limparFormulario() {
+formTarefa.reset();
+document.getElementById('tarefa-id').value = '';
+
+formTitulo.textContent = "Salvar Tarefa";
+btnSalvarText.textContent = "Salvar Tarefa";
+btnCancelar.classList.add('hidden');
+
+}
+
+function mostrarToast(mensagem, tipo = 'success') {
+const container = document.getElementById('toast-container');
+const toast = document.createElement('div');
+
+const cores = {
+    success: 'bg-green-600',
+    error: 'bg-rose-600',
+    info: 'bg-blue-600'
+};
+
+const icones = {
+    success: 'fa-check-circle',
+    error: 'fa-exclamation-triangle',
+    info: 'fa-info-circle'
+};
+
+toast.className = `toast-enter ${cores[tipo]} toast-message`;
+toast.innerHTML = `
+    <i class="fas ${icones[tipo]} text-lg"></i>
+    <span>${mensagem}</span>
+`;
+
+container.appendChild(toast);
+
+setTimeout(() => {
+    toast.style.opacity = '0';
+    toast.style.transform = 'translateX(100%)';
+    toast.style.transition = 'all 0.3s ease';
+
+    setTimeout(() => toast.remove(), 300);
+}, 3000);
+
+}
+
+//=================================================
+// Ler dados do Banco e Popular a Tabela
+//=================================================
+
+async function criarTabelaTarefas() {
+const dados = await consultarDiretoComFetch();
+
+tabelaTarefa.innerHTML = '';
+
+if (!dados || dados.length === 0) {
+    tabelaTarefa.innerHTML = `
+        <tr>
+            <td colspan="4" class="empty-state">
+                Nenhuma tarefa encontrada
+            </td>
+        </tr>
+    `;
+    return;
+}
+
+
+dados.forEach(tarefa => {
+    const linha = document.createElement('tr');
+
+        const criado_em = new Date(tarefa.criado_em).toLocaleString('pt-BR', {
+        day: '2-digit',
+        month: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false,
+        }).replace(',', '')
+
+
+    linha.innerHTML = `
+        <td class="cell-id">#${tarefa.id}</td>
+
+        <td>
+            <p class="cell-tarefa">${tarefa.titulo}</p>
+            <p class="cell-descricao">${tarefa.descricao}</p>
+        </td>
+    
+        <td class="date"><p class="badge-date">${criado_em}</p></td>
+
+        <td>
+            <div class="action-container">
+                <button
+                    onclick="prepararEdicao(${tarefa.id}, '${tarefa.titulo}', '${tarefa.descricao}', '${tarefa.criado_em}')"
+                    class="btn-action btn-edit"
+                    title="Editar">
+                    <i class="fas fa-pen"></i>
+                </button>
+
+                <button
+                    onclick="deletarTarefa(${tarefa.id})"
+                    class="btn-action btn-delete"
+                    title="Excluir">
+                    <i class="fas fa-trash"></i>
+                </button>
+            </div>
+        </td>
+    `;
+
+    tabelaTarefa.appendChild(linha);
+});
+
+}
+
+window.criarTabelaTarefas = criarTabelaTarefas;
+
+criarTabelaTarefas();
+
+window.prepararEdicao = function (id, titulo, descricao, criado_em) {
+document.getElementById('tarefa-id').value = id;
+document.getElementById('tarefa-titulo').value = titulo;
+document.getElementById('tarefa-descricao').value = descricao;
+
+const campoData = document.getElementById('tarefa-criado_em');
+if (campoData) {
+    campoData.value = criado_em;
+}
+
+formTitulo.textContent = "Editar Tarefas";
+btnSalvarText.textContent = "Atualizar Tarefas";
+btnCancelar.classList.remove('hidden');
+
+};
+
+async function lidarComEnvioDoFormulario(event) {
+event.preventDefault();
+
+const id = document.getElementById('tarefa-id').value;
+const titulo = document.getElementById('tarefa-titulo').value;
+const descricao = document.getElementById('tarefa-descricao').value;
+
+let sucesso = false;
+
+if (id) {
+    sucesso = await sqlAtualizarTarefa(id, titulo, descricao);
+
+    if (sucesso) {
+        mostrarToast("Tarefa atualizada com sucesso!", "success");
+    }
+} else {
+    sucesso = await insertTarefa(titulo, descricao);
+
+    if (sucesso) {
+        mostrarToast("Tarefa cadastrada com sucesso!", "success");
+    }
+}
+
+if (sucesso) {
+    limparFormulario();
+    criarTabelaTarefas();
+}
+
+}
+
+window.deletarTarefa = async function(id) {
+if (confirm("Tem certeza que deseja excluir essa tarefa?")) {
+const sucesso = await sqlDeletarTarefa(id);
+
+    if (sucesso) {
+        mostrarToast("Tarefa excluída com sucesso!", "success");
+        criarTabelaTarefas();
+    } else {
+        mostrarToast("Erro ao excluir tarefa!", "error");
+    }
+}
+
+};
+
+formTarefa.addEventListener('submit', lidarComEnvioDoFormulario);
+btnCancelar.addEventListener('click', limparFormulario);
---
+++ script_db.js
@@ -0,0 +1,109 @@
+const DATABASE_URL = "postgresql://neondb_owner:npg_iSmjb58RgQLN@ep-dry-butterfly-acofm1zs-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+
+const host = new URL(DATABASE_URL).host.replace('-pooler', '');
+
+const neonHttpEndPoint = `https://${host}/sql`;
+
+const NEON_API_TOKEN ="https://ep-dry-butterfly-acofm1zs.apirest.sa-east-1.aws.neon.tech/neondb/rest/v1";
+
+async function executarQueryNeon(querySQL, parametros = []) {
+    try {
+        
+        //Requisição de conexão com o banco
+        const resposta = await fetch(neonHttpEndPoint, {
+
+            // Header da Requisição
+            method: 'POST',
+            headers: { 
+                'neon-connection-string': DATABASE_URL,
+                'Content-Type': 'application/json' // Corrigido 'aplication' para 'application'
+            },
+
+            //corpo da requisição
+            body: JSON.stringify({
+                query: querySQL, //SQL: Ex. INSERT INTO...
+                params: parametros 
+            })
+        });
+
+        if (!resposta.ok) {
+            // Resposta do Servidor Neon
+            const erroTexto = await resposta.text();
+
+            // joga o erro para o catch "pegar" e mostrar no console
+            throw new Error(`Erro HTTP ${resposta.status}: ${erroTexto}`);
+        }
+
+        const dados = await resposta.json() //Resposta do Neon com dados
+        return dados.rows; // Extraindo linhas dos dados
+      
+        // pega o erro gerado pelo catch
+    } catch (erro) {
+        console.error("Falha ao comunicar com o banco de dados: ", erro);
+        return null
+    }
+}
+
+// CRUD
+
+//c
+export async function insertTarefa(titulo, descricao) {
+    const query = 'INSERT INTO tarefas (titulo, descricao) VALUES ($1, $2) RETURNING *';
+
+    const params = [titulo, descricao]
+
+        // Linhas que o RETURNING irá retornar
+    const linhas = await executarQueryNeon(query, params);
+    
+    // Retorna um booleano se tem linhas true ou false
+    return linhas !== null;
+};
+
+//r
+export async function consultarDiretoComFetch() {
+    
+    const query = 'SELECT * FROM tarefas ORDER BY criado_em DESC';
+
+        //Executa as linhas que a query select vai retornar chamando a função executarQueryNeon
+    const linhas = await executarQueryNeon(query);
+
+    //Garante a função volte um Array mesmo se vazio
+    return linhas || [];
+};
+
+//U
+export async function sqlAtualizarTarefa(id, titulo, descricao) {
+    const query = 'UPDATE tarefas SET titulo = $1, descricao = $2 WHERE id = $3 RETURNING *';
+
+    const params = [titulo, descricao, id];
+
+    // Executa as linhas que a query de update vai retornar chamando a função executarQueryNeon
+    const linhas = await executarQueryNeon(query, params);
+
+    // Retorna um booleano se tem linhas true ou false
+    return linhas !== null; 
+};
+
+//D
+export async function sqlDeletarTarefa(id) {
+    const query = 'DELETE FROM tarefas where id = $1 RETURNING *'
+
+    //Parametros para o Delete
+    const params = [id];
+
+    // Executa as linhas que a query de delete vai retornar chamando a função executarQueryNeon
+    const linhas = await executarQueryNeon(query, params);
+
+     // Retorna um booleano se tem linhas true ou false
+    return linhas !== null;
+}
+
+export async function timestamp(criado_em) {
+
+    const params = [criado_em]
+    const query = 'SELECT * FROM tarefas WHERE criado_em IS NOT NULL'
+    const linhas = await executarQueryNeon(query)
+
+    return linhas || [];
+}
+
